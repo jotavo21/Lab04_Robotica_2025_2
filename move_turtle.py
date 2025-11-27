041d1d775f94105a2f9e3a3b006ceaf274be2fa1
Juan Otavo <67178017+jotavo21@users.noreply.github.com>
2025-11-26 20:54:15 -0500

    Update move_turtle.py

--- move_turtle.py
+++ move_turtle.py
@@ -5,44 +5,63 @@ from std_srvs.srv import Empty
 import curses
 import time
 
+
 class TurtleTeleop(Node):
     def __init__(self, screen):
-        super().__init__("turtle_teleop_letters_reset")
+        super().__init__("turtle_controller")
 
         self.pub = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.reset_cli = self.create_client(Empty, "/reset")
-
-        while not self.reset_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("Esperando servicio /reset ...")
-
         self.linear_speed = 1.5
         self.angular_speed = 1.45
-
         self.screen = screen
         self.screen.nodelay(True)
         curses.cbreak()
 
+        # Timer que lee el teclado periódicamente
         self.timer = self.create_timer(0.1, self.read_keyboard)
 
-        self.get_logger().info("Teleop listo. Flechas = mover. Letras = dibujar con reset (I S R P J E O G).")
+        self.get_logger().info(
+            "Moviendo la tortuga: Flechas verticales = Avanzar o Retroceder. Flechas horizontales = Girar. Letras = Dibujar la letra (I S R P J E O G)."
+        )
 
     # ----------------------------
-    # RESET
+    # UTILIDADES DE TECLADO
     # ----------------------------
+    def _drain_keys(self):
+        #Elimina las teclas que se acumulan la cola de entradas de teclado (FUnciona con todas las teclas)
+        while True:
+            ch = self.screen.getch()
+            if ch == -1:
+                break
+
+    def _get_last_key(self):
+        
+        #Devuelve solo la última tecla de la cola de entradas de teclado, para evitar acumulacion (Principalmente para corregir errores de las entradas por flechas)
+        last = -1
+        while True:
+            ch = self.screen.getch()
+            if ch == -1:
+                break
+            last = ch
+        return last
+
+ 
+    #Resetear la posicion de la tortuga
     def reset_turtle(self):
         req = Empty.Request()
         self.reset_cli.call_async(req)
         time.sleep(0.2)
 
-    # ----------------------------
-    # MOVIMIENTOS PRIMITIVOS
-    # ----------------------------
+
+    # Avance, retroceso, giro horario y antihorario con la velocidad establecida por el tiempo determinado
     def move_forward(self, t):
         msg = Twist()
         msg.linear.x = self.linear_speed
         start = time.time()
         while time.time() - start < t:
             self.pub.publish(msg)
+            self._drain_keys() 
             time.sleep(0.01)
         self.stop()
 
@@ -52,6 +71,7 @@ class TurtleTeleop(Node):
         start = time.time()
         while time.time() - start < t:
             self.pub.publish(msg)
+            self._drain_keys()
             time.sleep(0.01)
         self.stop()
 
@@ -61,6 +81,7 @@ class TurtleTeleop(Node):
         start = time.time()
         while time.time() - start < t:
             self.pub.publish(msg)
+            self._drain_keys()
             time.sleep(0.01)
         self.stop()
 
@@ -70,6 +91,7 @@ class TurtleTeleop(Node):
         start = time.time()
         while time.time() - start < t:
             self.pub.publish(msg)
+            self._drain_keys()
             time.sleep(0.01)
         self.stop()
 
@@ -77,34 +99,34 @@ class TurtleTeleop(Node):
         msg = Twist()
         self.pub.publish(msg)
 
-    # ----------------------------
-    # LETRAS SOLO CON LÍNEAS RECTAS
-    # ----------------------------
 
-    # todas las letras empiezan igual: reset + un punto base
+    #Dibujo de las iniciales de los integrantes
 
     def draw_I(self):
         self.get_logger().info("Dibujando I")
+        self._drain_keys()        
         self.reset_turtle()
         self.turn_left(1)
         self.move_forward(2)
 
     def draw_S(self):
         self.get_logger().info("Dibujando S")
+        self._drain_keys()
         self.reset_turtle()
 
-        self.move_forward(1.5)         # ─
-        self.turn_left(1)              # │
+        self.move_forward(1.5)         
+        self.turn_left(1)              
         self.move_forward(1.5)
-        self.turn_left(1)              # ─
+        self.turn_left(1)              
         self.move_forward(1.5)
-        self.turn_right(1)             # │
+        self.turn_right(1)             
         self.move_forward(1.5)
         self.turn_right(1)
         self.move_forward(1.5)
 
     def draw_R(self):
         self.get_logger().info("Dibujando R")
+        self._drain_keys()
         self.reset_turtle()
 
         self.turn_left(1)
@@ -120,6 +142,7 @@ class TurtleTeleop(Node):
 
     def draw_P(self):
         self.get_logger().info("Dibujando P")
+        self._drain_keys()
         self.reset_turtle()
 
         self.turn_left(1)
@@ -133,61 +156,73 @@ class TurtleTeleop(Node):
 
     def draw_J(self):
         self.get_logger().info("Dibujando J")
+        self._drain_keys()
         self.reset_turtle()
 
-        self.turn_left(1)
-        self.turn_right(1)
-        self.move_forward(2)
-        self.turn_left(1)
-        self.move_forward(3)
-        self.turn_left(1)
         self.move_forward(1)
+        self.turn_left(1.1)
+        self.move_forward(2)
+        self.turn_left(1.2)
+        self.move_forward(0.8)
+        self.move_backward(1.6)
 
     def draw_E(self):
         self.get_logger().info("Dibujando E")
+        self._drain_keys()
         self.reset_turtle()
 
+        self.turn_left(1.05)
+        self.move_forward(3)      
+        self.turn_right(1.03)
+        
+        self.move_forward(1.5)    
+        self.move_backward(1.5)
+        self.turn_left(1.03)
+        for _ in range(2):
+            self.move_backward(1.5)    
+            self.move_forward(1.5)      
+            self.move_backward(1.5)
+            self.turn_left(1.03)
+
+    def draw_O(self):
+        self.get_logger().info("Dibujando O")
+        self._drain_keys()
+        self.reset_turtle()
+        self.turn_left(1)
+        msg = Twist()
+        msg.angular.z = 1.0
+        for _ in range(4):
+            self.move_forward(1.8)
+    def draw_G(self):
+        self.get_logger().info("Dibujando G")
+        self._drain_keys()
+        self.reset_turtle()
         self.turn_left(1)
         self.move_forward(3)
         self.turn_right(1)
         self.move_forward(1.5)
-        self.turn_backward = 1
-        self.turn_left(1)
         self.move_backward(1.5)
         self.turn_left(1)
+        self.move_backward(3)
+        self.turn_right(1)
         self.move_forward(1.5)
-        self.turn_right(1)
-        self.move_forward(1)
 
-    def draw_O(self):
-        self.get_logger().info("Dibujando O")
-        self.reset_turtle()
-        self.turn_left(1)
-
-        for _ in range(4):
-            self.move_forward(2)
+        for _ in range(2):
             self.turn_left(1)
+            self.move_forward(1)
+        self.turn_left(1.23)
+        self.move_forward(0.5)
 
-    def draw_G(self):
-        self.get_logger().info("Dibujando G")
-        self.reset_turtle()
 
-        self.turn_left(1)
-        for _ in range(3):
-            self.move_forward(2)
-            self.turn_left(1)
-        self.move_forward(2)
-        self.turn_right(1)
-        self.move_forward(1)
+    
+    #Lectura de teclado
 
-    # ----------------------------
-    # LECTURA DE TECLADO
-    # ----------------------------
     def read_keyboard(self):
-        key = self.screen.getch()
+        # Leer solo la última tecla pulsada desde el último ciclo
+        key = self._get_last_key()
         msg = Twist()
 
-        # Movimiento normal
+        # Movimiento normal con flechas
         if key == curses.KEY_UP:
             msg.linear.x = self.linear_speed
         elif key == curses.KEY_DOWN:
@@ -202,15 +237,23 @@ class TurtleTeleop(Node):
 
         self.pub.publish(msg)
 
-        # Letras con reset
-        if key in [ord("I"), ord("i")]: self.draw_I()
-        if key in [ord("S"), ord("s")]: self.draw_S()
-        if key in [ord("R"), ord("r")]: self.draw_R()
-        if key in [ord("P"), ord("p")]: self.draw_P()
-        if key in [ord("J"), ord("j")]: self.draw_J()
-        if key in [ord("E"), ord("e")]: self.draw_E()
-        if key in [ord("O"), ord("o")]: self.draw_O()
-        if key in [ord("G"), ord("g")]: self.draw_G()
+        # Dibujo de letras
+        if key in [ord("I"), ord("i")]:
+            self.draw_I()
+        if key in [ord("S"), ord("s")]:
+            self.draw_S()
+        if key in [ord("R"), ord("r")]:
+            self.draw_R()
+        if key in [ord("P"), ord("p")]:
+            self.draw_P()
+        if key in [ord("J"), ord("j")]:
+            self.draw_J()
+        if key in [ord("E"), ord("e")]:
+            self.draw_E()
+        if key in [ord("O"), ord("o")]:
+            self.draw_O()
+        if key in [ord("G"), ord("g")]:
+            self.draw_G()
 
 
 def main():
